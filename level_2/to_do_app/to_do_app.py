d0d45de9f3fcc1047ff811f114dad1e126481ef7
AgawuKakraba <thomas.agawukakrabaf3@gmail.com>
2025-09-19 21:16:37 +0000

    to_do_app edit

--- level_2/to_do_app/to_do_app.py
+++ level_2/to_do_app/to_do_app.py
@@ -1,11 +1,11 @@
 import json
 #Loading or creating the JSON file if it doesn't exist
 try:
-    with open("level_2/tasks.json", "r") as f:
+    with open("level_2/to_do_app/tasks.json", "r") as f:
         tasks = json.load(f)
 except (FileNotFoundError, json.JSONDecodeError):
     tasks = {}
-    with open("level_2/tasks.json", "w") as f:
+    with open("level_2/to_do_app/tasks.json", "w") as f:
         json.dump(tasks, f)
 
 #Main loop for the to-do list application
@@ -39,7 +39,7 @@ while True:
             print("Task already exists.")
         else:
             tasks[new_task] = False
-            with open("level_2/tasks.json", "w") as f:
+            with open("level_2/to_do_app/tasks.json", "w") as f:
                 json.dump(tasks, f)
             print(f"Task '{new_task}' added.")
     elif choice == '3':
@@ -47,12 +47,12 @@ while True:
         del_task = input("Enter the task to remove('all' to remove all tasks): ")
         if del_task in tasks:
             del tasks[del_task]
-            with open("level_2/tasks.json", "w") as f:
+            with open("level_2/to_do_app/tasks.json", "w") as f:
                 json.dump(tasks, f)
             print(f"Task '{del_task}' removed.")
         elif del_task == "all":
             tasks.clear()
-            with open("level_2/tasks.json", "w") as f:
+            with open("level_2/to_do_app/tasks.json", "w") as f:
                 json.dump(tasks, f)
             print("All tasks removed.")
         else:
@@ -62,7 +62,7 @@ while True:
         comp_task = input("Enter the task to mark as completed: ")
         if comp_task in tasks:
             tasks[comp_task] = True
-            with open("level_2/tasks.json", "w") as f:
+            with open("level_2/to_do_app/tasks.json", "w") as f:
                 json.dump(tasks, f)
             print(f"Task '{comp_task}' marked as completed.")
         else:
